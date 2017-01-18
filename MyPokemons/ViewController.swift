b75b34d35ca287ec7e90b66a61f5e3f875541568
Lucheo Antonio Tombini Filho <lucheo@Lucheos-MacBook-Pro.local>
2017-01-18 13:44:27 -0200

    Initial Commit

---
+++ MyPokemons/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyPokemons
+//
+//  Created by Lucheo Antonio Tombini Filho on 18/01/17.
+//  Copyright © 2017 Lucheo Antonio Tombini Filho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
