455383fb9376698e4c79c47139bd16d01a16c6c6
Lucheo Antonio Tombini Filho <lucheo@Lucheos-MacBook-Pro.local>
2017-01-19 11:49:40 -0200

    Segue already in place

--- MyPokemons/PokeCell.swift
+++ MyPokemons/PokeCell.swift
@@ -13,12 +13,21 @@ class PokeCell: UICollectionViewCell {
     @IBOutlet weak var nameLbl: UILabel!
     
     var pokemon: Pokemon!
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 8.0
+        
+    }
     
-    func configureCell(pokemon: Pokemon) {
+    
+    func configureCell(_ pokemon: Pokemon) {
         self.pokemon = pokemon
         nameLbl.text = self.pokemon.name.capitalized
         pokemonImg.image = UIImage(named: "\(self.pokemon.pokeId)")
         
     }
+    
+    
     
 }
---
+++ MyPokemons/PokemonDetailVC.swift
@@ -0,0 +1,18 @@
+//
+//  PokemonDetailVC.swift
+//  MyPokemons
+//
+//  Created by Lucheo Antonio Tombini Filho on 19/01/17.
+//  Copyright © 2017 Lucheo Antonio Tombini Filho. All rights reserved.
+//
+
+import UIKit
+
+class PokemonDetailVC: UIViewController {
+    
+    var pokemon: Pokemon!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
